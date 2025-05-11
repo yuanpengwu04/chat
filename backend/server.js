5658fc3e83a6e687e0122503b50ec53a013fcb52
YYY1124 <1155177150@link.cuhk.edu.hk>
2025-05-11 22:53:28 +0800

    /api/conversation added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import licenseRoutes from "./routes/license.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import conversationRoutes from "./routes/conversation.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -21,6 +22,7 @@ app.use("/api/license", licenseRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/conversations", conversationRoutes);
 
 server.listen(PORT, () => {
   connectToMongoDB();
