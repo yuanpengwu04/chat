363405ddf8ea900409cf9b60ee4bd8baca886415
yuanpengwu04 <yuanpengwu04@gmail.com>
2025-05-11 23:12:40 +0800

    production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import licenseRoutes from "./routes/license.routes.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -12,6 +13,8 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+const __dirname = path.resolve();
+console.log(__dirname);
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -21,6 +24,12 @@ app.use("/api/license", licenseRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
