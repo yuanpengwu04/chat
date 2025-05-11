2eed3eb4c9f325437a3018e6f07e05abd5e91674
YYY1124 <1155177150@link.cuhk.edu.hk>
2025-05-11 22:54:10 +0800

    Change the logic of useGetConversation

--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useGetConversations = () => {
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(false);
   const [conversations, setConversations] = useState([]);
+  const { authUser } = useAuthContext();
+
   useEffect(() => {
     const getConversations = async () => {
+      if (!authUser) return;
+
       setLoading(true);
       try {
-        const res = await fetch("/api/users");
+        const res = await fetch(`/api/conversations/${authUser._id}`, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include", // This ensures cookies are sent with the request
+        });
         const data = await res.json();
 
         if (data.error) {
@@ -22,7 +32,7 @@ const useGetConversations = () => {
       }
     };
     getConversations();
-  }, []);
+  }, [authUser]); // Add authUser as a dependency
 
   return { loading, conversations };
 };
